a72575fe965fc37cff960707fcade846be397719
sergio Gerardo Magno <diga.magno@hotmail.com>
2020-09-18 00:28:23 -0300

    correct Routes switch

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Switch from 'react-bootstrap/esm/Switch';
-import { Redirect, Route } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import CreatePost from './CreatePost';
 import Details from './Details';
 import EditPost from './EditPost';
 import Header from './Header';
 import Home from './Home';
+import NotFound from './NotFound';
 
 export default ()=>{
     return(
@@ -19,6 +19,7 @@ export default ()=>{
                 <Route exact path='/home/:id' component={Details}/>
                 <Route exact path='/editPost' component={EditPost}/>
                 <Route exact path='/newPost' component={CreatePost}/>
+                <Route component={NotFound}/>
             </Switch>
         </div>
     )
