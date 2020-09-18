44d62ffa31837b45a3bfc0cf627b4845194929c2
sergio Gerardo Magno <diga.magno@hotmail.com>
2020-09-17 23:25:05 -0300

    add CreatePost route

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Switch from 'react-bootstrap/esm/Switch';
 import { Redirect, Route } from 'react-router';
+import CreatePost from './CreatePost';
 import Details from './Details';
 import EditPost from './EditPost';
 import Header from './Header';
@@ -17,6 +18,7 @@ export default ()=>{
                 <Route exact path='/home' component={Home}/>
                 <Route exact path='/home/:id' component={Details}/>
                 <Route exact path='/editPost' component={EditPost}/>
+                <Route exact path='/newPost' component={CreatePost}/>
             </Switch>
         </div>
     )
