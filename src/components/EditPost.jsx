1702c15b6fe9bfce15bd53c5b5c425d60c8949f6
sergio Gerardo Magno <diga.magno@hotmail.com>
2020-09-18 22:40:14 -0300

    correct EditPost

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -32,7 +32,7 @@ class EditPost extends React.Component{
                 title: this.props.editPost.title,
                 body: this.props.editPost.body,
                 postPosition: this.props.postsList.indexOf(this.props.editPost)
-            })
+            }, ()=>{ console.log(this.state)})
         } catch (error) {
             return    
         }   
@@ -45,6 +45,7 @@ class EditPost extends React.Component{
             if(element.id === id){ 
                 post = element
                 this.setState({
+                    errors: {},
                     title: element.title,
                     body: element.body,
                     postPosition: this.props.postsList.indexOf(element)
@@ -56,11 +57,14 @@ class EditPost extends React.Component{
     }
 
     handleChange = ({target})=>{
+        console.log('change')
+        console.log(target)
         const{ name, value } = target
-        this.setState({ [name]: value })
+        this.setState({ [name]: value }, ()=>{ console.log(this.state)})
     }
 
     handleSubmit = (e)=>{
+        console.log(this.state)
         e.preventDefault();
         this.setState({send: false})
         const { errors, send, postPosition, ...stateValues} = this.state;
@@ -81,6 +85,9 @@ class EditPost extends React.Component{
     render(){
         const { errors } = this.state;
         const { send } = this.state;
+        const { title } = this.state;
+        const { body } = this.state;
+        const { id } = this.state;
         return(
             <Container className='shadow p-4'>
                 {send && 
@@ -92,7 +99,7 @@ class EditPost extends React.Component{
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Group>
                         <Form.Label>ID</Form.Label>
-                        <Form.Control as="select" name='id' onChange={this.handlePost} defaultValue={ this.props.editPost ? this.props.editPost.id : ''} disabled={this.props.editPost ? true : false} className={`form-control ${!errors.id ? '' : 'is-invalid' } `}  >
+                        <Form.Control as="select" name='id' onChange={this.handlePost} value={ this.props.editPost ? id : ''}  className={`form-control ${!errors.id ? '' : 'is-invalid' } `}  >
                             <option></option>
                             {this.props.postsList.map( element =>  <option key={element.id}> {element.id} </option> )}
                         </Form.Control>
@@ -100,12 +107,12 @@ class EditPost extends React.Component{
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className='mt-3'> Title </Form.Label>
-                        <Form.Control onChange={this.handleChange} type='text' name='title' defaultValue={ this.props.editPost ? this.props.editPost.title : ''} className={`form-control ${!errors.title ? '' : 'is-invalid' } `} />
+                        <Form.Control onChange={this.handleChange} type='text' name='title' value={ this.props.editPost ? title : ''} className={`form-control ${!errors.title ? '' : 'is-invalid' } `} />
                         {errors.title &&  <div className="invalid-feedback "> {errors.title} </div> }
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className='mt-3'> Body </Form.Label>
-                        <Form.Control as="textarea" rows="5" name='body' onChange={this.handleChange} defaultValue={ this.props.editPost ? this.props.editPost.body : ''} className={`form-control ${!errors.body ? '' : 'is-invalid' } `} />
+                        <Form.Control as="textarea" rows="5" name='body' onChange={this.handleChange} value={ this.props.editPost ? body : ''} className={`form-control ${!errors.body ? '' : 'is-invalid' } `} />
                         {errors.body &&  <div className="invalid-feedback "> {errors.body} </div> }
                     </Form.Group>
                     <Button variant="dark" type="submit">
@@ -122,17 +129,17 @@ class EditPost extends React.Component{
     }
 }
 
-function mapStateToProps(state){
-    return {
-        postsList: state.postsList,
-        editPost: state.editPost
-    }
-}
-
 function mapDispatchToProps(dispatch){
     return {
         setEditPost: (post) => dispatch(actions.setEditPostAction(post)),
         updatePost: (post, postPosition) => dispatch(actions.updatePostAction(post, postPosition))
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        postsList: state.postsList,
+        editPost: state.editPost
     }
 }
 
